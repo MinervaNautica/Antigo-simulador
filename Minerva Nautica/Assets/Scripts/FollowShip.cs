1499ccbf0b10456f26a6d7459b96b4ab5a077636
mandyfs <eng.amanda0@poli.ufrj.br>
2020-04-30 14:14:01 -0300

    a camera pode seguir o barco

---
+++ Minerva Nautica/Assets/Scripts/FollowShip.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowShip : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset = new Vector3(0, 1.5f, -1.5f);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
